13d0652da01e67bb13b4faa27ccd6ae2cf093e13
MarkNahabedian <naha@mit.edu>
2020-03-15 16:52:55 -0400

    Some code refactoring.  Show test viewbox.

--- cleanup_inkscape_svg.py
+++ cleanup_inkscape_svg.py
@@ -94,9 +94,10 @@ def extract_styles(doc, stylemap={}):
 
 
 def add_stylesheet(doc, stylemap):
-    style = doc.createElement("style")
-    docelt = doc.documentElement
-    docelt.insertBefore(style, docelt.firstChild)
+    '''Updates the stylesheet inkscape_styles (creating it if not present)
+    with tyle styles from stylemap.
+    '''
+    style = ensure_stylesheet(doc, "inkscape_styles")
     classnames = []
     for v in stylemap.values():
         if isinstance(v, str):
@@ -104,32 +105,68 @@ def add_stylesheet(doc, stylemap):
     classnames.sort()
     for c in classnames:
         s = stylemap[c]
-        style.appendChild(doc.createTextNode(
-            ".%s\t{ %s }" % (c, s.getCssText(" "))))
+        add_srtylesheet_rule(doc, style,
+                             "." + c,
+                             s.getCssText(" "))
+
+
+# *** TODO: getElementById isn't finding existing elements, presumably
+# because xml.dom doesn't know what the declared ID type attribute is
+# because it doesn't have the DTD.
+# The SVG namespace URI doesn't point to an XML schema.
+def ensure_stylesheet(doc, id):
+    '''Ensure that doc has a styleseet with id id and return it.'''
+    style = doc.getElementById(id)
+    if not style:
+        style = new_stylesheet(doc)
+        style.setAttribute("ID", id)
+    return style
+
+
+def new_stylesheet(doc):
+    style = doc.createElement("style")
+    docelt = doc.documentElement
+    docelt.insertBefore(style, docelt.firstChild)
+    return style
+
+
+def add_srtylesheet_rule(doc, style, selector, properties):
+    style.appendChild(doc.createTextNode("%s\t{ %s; }" %
+                                         (selector, properties)))
+
+
+def svg_line(doc, parent, x0, y0, x1, y1):
+    p = doc.createElement("path")
+    parent.appendChild(p)
+    p.setAttribute("d", "M %d %d L %d %d" % (
+        x0, y0, x1, y1))
+    return p
 
 
 def add_grid(doc, minX, minY, width, height):
-    maxX = minX + width
-    maxY = minY + height
+    maxX = minX + width - 1
+    maxY = minY + height - 1
     grid = doc.createElement("g")
     grid.setAttribute("class", "viewportGrid")
     doc.documentElement.appendChild(grid)
     for x in range(minX, maxX, 100):
-        p = doc.createElement("path")
-        grid.appendChild(p)
-        p.setAttribute("d", "M %d %d V %d" % (
-            x, minY, maxY))
+        svg_line(doc, grid, x, minY, x, maxY)
     for y in range(minY, maxY, 100):
-        p = doc.createElement("path")
-        grid.appendChild(p)
-        p.setAttribute("d", "M %d %d H %d" % (
-            minX, y, maxX))
-    style = doc.createElement("style")
-    docelt = doc.documentElement
-    docelt.insertBefore(style, docelt.firstChild)
-    style.appendChild(doc.createTextNode(".%s\t{ %s; }" %
-                                         ("viewportGrid", "stroke: blue")))
-    pass
+        svg_line(doc, grid, minX, y, maxX, y)
+    style = ensure_stylesheet(doc, "decorations")
+    add_srtylesheet_rule(doc, style, ".viewportGrid", "stroke: blue")
+
+
+def test_viewbox(doc, minX, minY, maxX, maxY):
+    box = doc.createElement("g")
+    box.setAttribute("class", "testViewBox")
+    doc.documentElement.appendChild(box)
+    svg_line(doc, box, minX, minY, maxX, minY)
+    svg_line(doc, box, minX, maxY, maxX, maxY)
+    svg_line(doc, box, minX, minY, minX, maxY)
+    svg_line(doc, box, maxX, minY, maxX, maxY)
+    style = ensure_stylesheet(doc, "decorations")
+    add_srtylesheet_rule(doc, style, ".testViewBox", "stroke: green")
 
 
 def main():
@@ -139,6 +176,7 @@ def main():
     viewbox = [ int(x) for x in doc.documentElement.getAttribute("viewBox").split()]
     print("viewBox", viewbox)
     add_grid(doc, *viewbox)
+    test_viewbox(doc, 700, 450, 970, 610)
     # Count elements
     element_counts = Counter()
     def counter(elt):
