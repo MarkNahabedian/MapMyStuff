d897a2ef95dbe09f932178105c81bda1815cd0be
MarkNahabedian <naha@mit.edu>
2020-04-01 15:02:22 -0400

    Some looser changed the argument list of line_intersects without updating the tests.

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 #  python -m unittest test
 
 import unittest
-from cleanup_inkscape_svg import Box
+from cleanup_inkscape_svg import Box, Transform
 from svg.path import Line
 
 
@@ -12,39 +12,48 @@ class TestBox (unittest.TestCase):
 
     def test_line_intersects_crosses(self):
         '''Line crosses Box with both end points outside.'''
-        self.assertTrue(self.box.line_intersects(Line(25+45j, 5+25j)))
+        self.assertTrue(self.box.line_intersects(Transform.identity(),
+                                                 Line(25+45j, 5+25j)))
 
     def test_line_intersects_crosses(self):
         '''Both end points inside Box.'''
-        self.assertTrue(self.box.line_intersects(Line(18+32j, 12+38j)))
+        self.assertTrue(self.box.line_intersects(Transform.identity(),
+                                                 Line(18+32j, 12+38j)))
 
     def test_line_start_inside_box(self):
         '''Start point inside Box.'''
-        self.assertTrue(self.box.line_intersects(Line(15+32j, 25+25j)))
+        self.assertTrue(self.box.line_intersects(Transform.identity(),
+                                                 Line(15+32j, 25+25j)))
 
-    def test_line_end_inside_box(self):        # *** FAILS
+    def test_line_end_inside_box(self):
         '''End point inside Box.'''
-        self.assertTrue(self.box.line_intersects(Line(25+25j, 15+32j)))
+        self.assertTrue(self.box.line_intersects(Transform.identity(),
+                                                 Line(25+25j, 15+32j)))
 
     def test_line_intersects_left(self):
         '''Line crosses Box but both end points are to the left of the box.'''
-        self.assertFalse(self.box.line_intersects(Line(0+33j, 8+35j)))
+        self.assertFalse(self.box.line_intersects(Transform.identity(),
+                                                  Line(0+33j, 8+35j)))
 
     def test_line_intersects_right(self):
         '''Line crosses Box but both end points are to the right of the box.'''
-        self.assertFalse(self.box.line_intersects(Line(22+33j, 28+35j)))
+        self.assertFalse(self.box.line_intersects(Transform.identity(),
+                                                  Line(22+33j, 28+35j)))
 
     def test_line_intersects_above(self):
         '''Line crosses Box but both end points are above the top of the box.'''
-        self.assertFalse(self.box.line_intersects(Line(5+25j, 12+28j)))
+        self.assertFalse(self.box.line_intersects(Transform.identity(),
+                                                  Line(5+25j, 12+28j)))
 
     def test_line_intersects_below(self):
         '''Line crosses Box but both end points are below the bottom of the box.'''
-        self.assertFalse(self.box.line_intersects(Line(15+42j, 8+50j)))
+        self.assertFalse(self.box.line_intersects(Transform.identity(),
+                                                  Line(15+42j, 8+50j)))
 
-    def test_line_intersects_nocross(self):     # *** FAILS
+    def test_line_intersects_nocross(self):
         '''Line does not cross Box.'''
-        self.assertFalse(self.box.line_intersects(Line(18+25j, 28+33j)))
+        self.assertFalse(self.box.line_intersects(Transform.identity(),
+                                                  Line(18+25j, 28+33j)))
 
 
 # I need a way to visualize the test cases:
