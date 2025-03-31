fd5c6f1649e9922ba9b1a8865f139644096aa86c
MarkNahabedian <naha@mit.edu>
2025-03-31 10:09:46 -0400

    A python script for verifying JSON files.  It can serve as a git pre-commit hook.

---
+++ git-hooks/pre-commit.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+# A precommit script to test that JSON files are without JSON syntax
+# error and only include supported properties.
+
+# To use this pre-commit hook in your local clone of the repository,
+# from your repository root do (on unix)
+#   ln -s git-hooks/pre-commit.py .git/hooks/pre-commit
+
+import os
+import sys
+import subprocess
+import json
+
+SUPPORTED_PROPERTIES = [
+    'name',
+    'unique_id',
+    'cssClass',
+    'clustermarket_id',
+    'width',
+    'depth',
+    'path_d',
+    'x',
+    'y',
+    'rotation',
+    'description',
+    'description_uri',
+    'contents',
+    'booking_note',
+    'measured'
+    ]
+
+def check_json_properties(data):
+    unknown = []
+    for d in data:
+        if isinstance(d, dict):
+            for property in d:
+                if not (property in SUPPORTED_PROPERTIES):
+                    if not (property in unknown):
+                        unknown.append(property)
+    return unknown
+
+
+def get_files():
+     cp = subprocess.run(["git", "diff", "--cached", "--name-only"],
+                         capture_output=True,
+                         text=True)
+     if cp.returncode != 0 or cp.stderr != "":
+         raise Exception("git diff failed: %s" % cp.stderr)
+         sys.exit(-1)
+     return [ s for s in cp.stdout.split("\n") if s ]
+     pass
+
+
+def check_json(filename):
+    if os.path.splitext(filename)[1] != ".json":
+        return True
+    with open(filename, 'r') as file:
+        try:
+            data = json.load(file)
+            unsupported = check_json_properties(data)
+            if unsupported:
+                print("%s: unsupported properties: %s" % (filename, unsupported))
+                return False
+        except Exception as e:
+            print("%s: %s" % (filename, e))
+            return False
+            pass
+        pass
+    return True
+    pass
+
+
+def main():
+    os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    print(os.getcwd())
+    if len(sys.argv) > 1:
+        files = sys.argv[1:]
+    else:
+        files = get_files()
+    failed = False
+    for file in files:
+        failed = check_json(file) or failed
+    if failed:
+        sys.exit(-1)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
+
